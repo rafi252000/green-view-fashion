79ddefca305195f6e6d4e8630f75bfffec2c268a
RafiUddin <rafiuddin.cse.pciu@gmail.com>
2026-02-26 01:36:03 +0600

    final code

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 text-2xl font-bold text-purple-700">
-            DevTriad Soft
+            Green View
           </div>
 
           {/* Desktop Links */}
